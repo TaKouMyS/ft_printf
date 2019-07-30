ae0e27dd0426b2f0b540f743eeba172b8c2bf43c
Allan Mamy <amamy@student.42.fr>
2019-07-30 22:28:53 +0200

    Update to include in libtf

--- libft/ft_putstrn.c
+++ libft/ft_putstrn.c
@@ -6,18 +6,11 @@
 /*   By: amamy <amamy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/22 13:50:10 by amamy             #+#    #+#             */
-/*   Updated: 2019/04/05 18:38:42 by amamy            ###   ########.fr       */
+/*   Updated: 2019/07/30 22:15:58 by amamy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	ft_putstr(char const *s)
-{
-	if (s == NULL)
-		return ;
-	write(1, s, ft_strlen(s));
-}
 
 void	ft_putstrn(char const *s, int len)
 {
